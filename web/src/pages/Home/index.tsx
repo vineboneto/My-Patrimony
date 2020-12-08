c6308fae727682f982cfa0c240f099d88f425b03
vineboneto <vineboneto@gmail.com>
2020-12-08 02:15:47 -0300

    created Home

---
+++ web/src/pages/Home/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+import okIcon from '../../assets/images/icons/okIcon.svg'
+import updateIcon from '../../assets/images/icons/updateIcon.svg'
+import searchIcon from '../../assets/images/icons/searchIcon.svg'
+
+import './styles.css'
+
+const Home: React.FC  = () => {
+    return (
+
+        <div id="page-patrimony-home">
+            <header className="page-patrimony-header">
+                <div className="top-bar-container">
+                    <span>Meu Patrimônio</span>    
+                </div>
+
+                <div className="header-content">
+                    <strong>Bem vindo</strong>
+                </div>
+            </header>
+
+            <main>
+                
+                <h1>O que você quer fazer ?</h1>
+                <div className="link-block">
+                    <Link to="/computer-form">
+                        Novo Patrimônio
+                        <img src={okIcon} alt="Novo Patrimônio"/>
+                    </Link>
+
+                    <Link to="/computer-form">
+                        Buscar Patrimônio
+                        <img src={searchIcon} alt="Buscar Patriônio"/>
+                    </Link>
+
+                    <Link to="/computer-form"> 
+                        Realizar Trocar
+                        <img src={updateIcon} alt="Realizar troca de patrimônio" />
+                    </Link>
+                </div>
+            </main>
+        </div>
+
+    )
+    
+
+
+}
+
+export default Home
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
+
 import ComputerForm from './pages/ComputerForm';
+import Home from './pages/Home';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Route path="/computer-form" component={ComputerForm} />
+            <Route exact path="/" component={Home} />
         </BrowserRouter>
     )
 }
