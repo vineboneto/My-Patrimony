76da4d174a329f74777a2f3316cd03ed265ed73d
vineboneto <vineboneto@gmail.com>
2020-12-21 00:55:07 -0300

    web: created search-block in Patrimony-list

--- web/src/pages/PatrimonyList/index.tsx
+++ web/src/pages/PatrimonyList/index.tsx
@@ -1,18 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Input from '../../components/Input'
 
 import PageHeader from '../../components/PageHeader'
+import Select from '../../components/Select'
 
 import './styles.css'
 
 const PatrimonyList: React.FC = () => {
+
+    const [owner, setOwner] = useState('')
+    const [sector, setSector] = useState('')
+    const [patrimony, setPatrimony] = useState('')
+
     return (
         <div id="page-patrimony-list">
             <PageHeader
                 title="O que procura?"
                 linkPrev="/"
-            >
+            />
 
-            </PageHeader>
+            <div className="search-block">
+                    <Select
+                        name="owner"
+                        label="Proprietário"
+                        value={owner}
+                        onChange={(e) => setOwner(e.target.value)}
+                        options={[
+                            { value: '1', label: 'Vinicius Boneto' }
+                        ]}
+                    />
+
+                    <Select
+                        name="sector"
+                        label="Setor"
+                        value={sector}
+                        onChange={(e) => setSector(e.target.value)}
+                        options={[
+                            { value: '1', label: 'UPA' }
+                        ]}
+                    />
+
+                    <Input
+                        name="patrimony"
+                        label="Patrimônio"
+                        value={patrimony}
+                        onChange={(e) => setPatrimony(e.target.value)}
+                    />
+
+                </div>
+
+
         </div>
         
     )
