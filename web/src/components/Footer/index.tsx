7209d82ac258735eeda0208c8a9046cfa3f64a1c
vineboneto <vineboneto@gmail.com>
2020-12-28 18:06:19 -0300

    web: new Layout to register

--- web/src/components/Footer/index.tsx
+++ web/src/components/Footer/index.tsx
@@ -7,42 +7,11 @@ import backIcon from '../../assets/images/icons/backIcon2.svg'
 
 import './styles.css'
 
-interface FooterProps {
-    toNext?: object
-    toPrev?: object
-    iconPrev?: string
-    iconNext?: string
-    labelButtonSave?: string
-    handleButton?: (e: MouseEvent) => void
-}
-
-const Footer: React.FC<FooterProps>  = ({ toNext, toPrev, iconNext, iconPrev, labelButtonSave, handleButton }) => {
+const Footer: React.FC  = ({ children }) => {
     return (
         <footer className="footer-block">
-            <div className="footer-content">
-                {toPrev  && 
-                    <div className="link">
-                        <Link to={toPrev || '/'}>
-                            <img src={backIcon} alt="Página anterior"/>
-                            <img src={iconPrev} alt={iconPrev}/>
-                        </Link>
-                    </div>
-                }
-                <div className="next-block">
-                    {handleButton &&
-                        <button onClick={(e) => handleButton(e)}>
-                            {labelButtonSave}
-                        </button>
-                    }
-                    {toNext && 
-                        <div className="link">
-                            <Link to={toNext || '/'} >
-                                <img src={iconNext} alt="Impressora"/>
-                                <img src={nextIcon} alt="Proxíma pagína"/>
-                            </Link>
-                        </div>
-                    }
-                </div>
+            <div className="footer-content">   
+                {children}
             </div>
         </footer>
     )
--- web/src/pages/PatrimonyForm/index.tsx
+++ web/src/pages/PatrimonyForm/index.tsx
@@ -1,24 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import React, { MouseEvent, useEffect, useState } from 'react'
 
 
 import PageHeader from '../../components/PageHeader'
+import IpItems from '../../components/IpItem'
 import Form from '../../components/Form'
 import Main from '../../components/Main'
 import Select from '../../components/Select'
+import Input from '../../components/Input'
+import Textarea from '../../components/Textarea'
+import Footer from '../../components/Footer'
+import api from '../../services/api'
 
 import './styles.css'
-import api from '../../services/api'
 
 const PatrimonyForm: React.FC = () => {
 
     const [sector, setSector] = useState('')
     const [owner, setOwner] = useState('')
+    const [type, setType] = useState('')
+    const [patrimony, setPatrimony] = useState('')
+    const [model, setModel] = useState('')
+    const [description, setDescription] = useState('')
+    const isIp = true
 
     const [optionsOwner, setOptionsOwner] = useState([
         { value: '', label: '', sectorId: '' }
     ])
     const [optionsSector, setOptionsSector] = useState([
         { value: '', label: '' }
+    ])
+
+    const [ipItems, setIpItems] = useState([
+        { ip: '', mask: '', gateway: '' }
     ])
 
     useEffect(() => {
@@ -52,6 +65,27 @@ const PatrimonyForm: React.FC = () => {
         setOptionsSector(options)
     }
 
+    async function handleCreatePatrimony(e: MouseEvent) {
+        e.preventDefault()
+    }
+
+    function setIpItemsValue(position: Number, field: string, value: string) {
+        const updateIpItems = ipItems.map((ipItem, index) => {
+            if (index === position) {
+                return { ...ipItem, [field]: value }
+            }
+            return ipItem
+        })
+        setIpItems(updateIpItems)
+    }
+
+    function addNewIpItem() {
+        setIpItems([
+            ...ipItems,
+            { ip: '', mask: '', gateway: '' }
+        ]);
+    }
+
 
 
     return (
@@ -77,10 +111,71 @@ const PatrimonyForm: React.FC = () => {
                                 label="Setor"
                                 options={optionsSector}
                                 value={sector}
-                                onChange={(e) => setSector(e.target.value)} />
+                                onChange={(e) => setSector(e.target.value)} 
+                            />
                         </div>
                     </Form>
 
+                    <Form
+                        legend="Patrimônio">
+                        <div className="patrimony-block">
+                            <Select
+                                name="type"
+                                label="Tipo"
+                                options={[
+                                    { value: '1', label: 'Computador' },
+                                    { value: '2', label: 'Impressora' }
+                                ]}
+                                value={type}
+                                onChange={(e) => setType(e.target.value)}
+                            />
+
+                            <Input
+                                name="patrimony"
+                                label="Patrimônio"
+                                value={patrimony}
+                                onChange={(e) => setPatrimony(e.target.value)}
+                            />
+
+                            <Input
+                                name="model"
+                                label="Modelo"
+                                value={model}
+                                onChange={(e) => setModel(e.target.value)}
+                            />
+                            <Textarea
+                                name="description"
+                                label="Descrição"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                            />
+                        </div>
+
+                    </Form>
+                    
+                    {isIp  &&
+                        <Form 
+                            legend="Ips"
+                            addNew={addNewIpItem}
+                            labelButton="+ Novo Ip">
+                            {ipItems.map((ipItem, index) => {
+                                return (
+                                    <IpItems
+                                        key={index}
+                                        ipItem={ipItem}
+                                        onIpChange={(ipValue: string) => setIpItemsValue(index, 'ip', ipValue) }
+                                        onMaskChange={(maskValue: string) => setIpItemsValue(index, 'mask', maskValue)} 
+                                        onGatewayChange={(gatewayValue: string) => setIpItemsValue(index, 'gateway', gatewayValue)} />
+                                )
+                            })}   
+                        </Form>
+                    }
+
+                    <Footer>
+                        <button onSubmit={() => handleCreatePatrimony}>
+                            Salvar Patrimônio
+                        </button>
+                    </Footer>   
                     
                 </Main>
         </div>
