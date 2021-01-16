8517e1fceca5f92dedc4676813216481612c7b14
vineboneto <vineboneto@gmail.com>
2021-01-16 15:32:51 -0300

    add commit msg linter

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -8,47 +8,47 @@ import searchIcon from '../../assets/images/icons/searchIcon.svg'
 
 import { Container, Header, TopBar, Title, Content, Actions } from './styled'
 
-const Home: React.FC  = () => {
-    return (
+const Home: React.FC = () => {
+	return (
 
-        <Container>
-            
-            <Header>
-                <TopBar>
-                    <span>Meu Patrimônio</span>    
-                </TopBar>
+		<Container>
 
-                <Title>
-                    <strong>Bem vindo</strong>
-                </Title>
-            </Header>
+			<Header>
+				<TopBar>
+					<span>Meu Patrimônio</span>
+				</TopBar>
 
-            <Content>
-                
-                <h1>O que você quer fazer ?</h1>
-                <Actions>
-                    <Link to="/patrimony/create">
-                        Novo Patrimônio
-                        <img src={okIcon} alt="Novo Patrimônio"/>
-                    </Link>
+				<Title>
+					<strong>Bem vindo</strong>
+				</Title>
+			</Header>
 
-                    <Link to={{
-                        pathname: '/patrimonies',
-                        search: 'page=1&limit=5'
-                    }}>
-                        Buscar Patrimônio
-                        <img src={searchIcon} alt="Buscar Patriônio"/>
-                    </Link>
+			<Content>
 
-                    <Link to="/patrimony/create"> 
-                        Realizar Trocar
+				<h1>O que você quer fazer ?</h1>
+				<Actions>
+					<Link to="/patrimony/create">
+						Novo Patrimônio
+                        <img src={okIcon} alt="Novo Patrimônio" />
+					</Link>
+
+					<Link to={{
+						pathname: '/patrimonies',
+						search: 'page=1&limit=5'
+					}}>
+						Buscar Patrimônio
+                        <img src={searchIcon} alt="Buscar Patriônio" />
+					</Link>
+
+					<Link to="/patrimony/create">
+						Realizar Trocar
                         <img src={updateIcon} alt="Realizar troca de patrimônio" />
-                    </Link>
-                </Actions>
-            </Content>
-        </Container>
+					</Link>
+				</Actions>
+			</Content>
+		</Container>
 
-    )
+	)
 }
 
 export default Home
