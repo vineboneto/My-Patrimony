8a579aab44f4672e58e92293c57e3d39099d88aa
vineboneto <vineboneto@gmail.com>
2021-01-19 10:25:53 -0300

    feat: created method create in OwnerController

--- server/src/controllers/OwnerController.ts
+++ server/src/controllers/OwnerController.ts
@@ -1,11 +1,34 @@
-import { PrismaClient, prismaVersion } from '@prisma/client';
-import { Request, Response } from 'express';
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
 
 const prisma = new PrismaClient();
 
 export default class OwnerController {
-	async index(req: Request, res: Response) {
-		const owners = prisma.owner.findMany();
-		return res.json(owners);
-	}
+  async index(req: Request, res: Response) {
+    const owners = prisma.owner.findMany();
+    return res.json(owners);
+  }
+
+  async create(req: Request, res: Response) {
+    const { name, sectorId } = req.body;
+    console.log(name);
+    console.log(sectorId);
+    try {
+      await prisma.owner.create({
+        data: {
+          name: name,
+          Sector: {
+            connect: {
+              id: sectorId,
+            },
+          },
+        },
+      });
+      return res.status(201).send();
+    } catch (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+  }
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,15 @@
-import express from 'express';
-import SectorController from './controllers/SectorController';
-import OwnerController from './controllers/OwnerController';
+import express from "express";
+import SectorController from "./controllers/SectorController";
+import OwnerController from "./controllers/OwnerController";
 
 const sectorController = new SectorController();
 const ownerController = new OwnerController();
 
 const routes = express.Router();
 
-routes.get('/sectors', sectorController.index);
-routes.post('/sectors', sectorController.create);
-routes.get('/owners', ownerController.index);
+routes.get("/sectors", sectorController.index);
+routes.post("/sectors", sectorController.create);
+routes.get("/owners", ownerController.index);
+routes.post("/owners", ownerController.create);
 
 export default routes;
